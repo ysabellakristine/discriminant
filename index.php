45cbe0e702c303a3b9f57d7baa2dcb3eab7a7a22
ysabellakristine <167633653+ysabellakristine@users.noreply.github.com>
2024-05-19 23:09:29 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,40 +1,22 @@
 <!DOCTYPE html>
 <html lang="en">
-	<head>
-		<title>Comic Sans About Myself</title>
-		<style> 
-		body { 
-			font-family: Comic Sans MS;
-			background-color: rgb(176,101,0);
-			line-height: 1.7;}
-		h1 {
-			color: rgb(135,206,250);}
-		h2 { 
-			color: rgb(139,0,0);}
-		p {
-			margin-bottom: 1em;}
-		.me-pic {
-			max-width: 100px; 
-            		max-height: auto;}
-		</style>
-	</head>
-	<body>
-		<h1> Greetings! </h1>
-		<img src="me.jpg" alt="Photo of me (please refresh the page if image is broken)" class="me-pic">
-		<p> Hi! I'm Ysabella Kristine B. Romero. I am a second year Computer Science student studying in Emilio Aguinaldo College Cavite. Here's some known facts about me ♡⸜(˶˃ ᵕ ˂˶)⸝♡</p>
-		<h1> Some facts about me </h1>
-		<ol>
-			<li> I have two dogs: <b> Robby and Molly </b> </li>
-			<li> I like reading on the <i> internet </i> </li>
-			<li> I play <b> games </b> </li>
-			<li> If you play with me on ML, be ready to hear me trashtalking our <b> opponents </b> and/or <b> teammates </b> <i>(but not you of course) </i> </li>
-		</ol>
-		<h2>Songs I listen to while i'm busy (on repeat)</h2>
-		<ul> 
-			<li> <a href="https://youtu.be/lsZkA0SZHN8?si=KEvylGwvRQDBWBpP"> Doing housework </a></li>
-			<li> <a href="https://youtu.be/AWPB2p1CEZQ?si=QxKkhYDPgA8wKHWQ"> Giving my dogs a bath </a></li>
-			<li> <a href="https://youtu.be/Blv9qVYlioY?si=bYIhTpOCDdRvRa_1"> When coding </a></li>
-		</ul>
-		<p> That's all! Thanks for reading! </p>
-	</body>
+<head>
+    <title>Discriminant Calculator</title>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+<body>
+<div class="container">
+    <h2>Calculating b^2 - 4ac </h2>
+    <form method="post" action="result.php">
+        <label for="a">a:</label>
+        <input type="number" id="a" name="a" required><br>
+        <label for="b">b:</label>
+        <input type="number" id="b" name="b" required><br>
+        <label for="c">c:</label>
+        <input type="number" id="c" name="c" required><br>
+        <button type="submit">Calculate</button>
+    </form>
+</div>
+
+</body>
 </html>
